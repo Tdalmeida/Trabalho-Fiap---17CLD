4b9a4c6ec7a4783a0b61a3a8d9152634e7e4510e
thiadal <thiagoa68@gmail.com>
2019-03-25 13:24:41 -0300

    deploy

---
+++ markpub-web/README.md
@@ -0,0 +1,4 @@
+# markpub-web
+
+TODO: Enter the cookbook description here.
+
---
+++ markpub-web/attributes/default.rb
@@ -0,0 +1,4 @@
+default["markpub-web_final"]["sites"]["markpub.dev.com"] = { "port" => 80, "servername" => "markpub.hom.com", "serveradmin" => "webmaster@markpub.com" }
+default["markpub-web_final"]["sites"]["markpub.hom.com"] = { "port" => 80, "servername" => "markpub.dev.com", "serveradmin" => "webmaster@markpub.com" }
+default["markpub-web_final"]["sites"]["markpub.prod.com"] = { "port" => 80, "servername" => "markpub.prod.com", "serveradmin" => "webmaster@markpub.com" }
+
